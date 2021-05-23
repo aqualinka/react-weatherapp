5acd0d952fd594defa806853edb28575b5a97801
aqualinka <michaela.kovaja@gmail.com>
2021-05-24 00:39:46 +0100

    udated the files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
-
+import "./App.css";
 import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
-      <Weather />
+      <div className="container">
+        <Weather defaultCity="Arrecife"/>
+        <div className="coder text-center">        
+          <a href="https://github.com/aqualinka/react-weatherapp" target="_blank" rel="noreferrer">open-source-code </a>by Michaela Kovacova and hosted on <a href="https://happy-wing-013044.netlify.app" target="_blank"rel="noreferrer">Netlify</a>
+        </div>
+      </div>
     </div>
   );
 }
