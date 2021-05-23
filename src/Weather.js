21cbe7401c9b9a99bb32bdaf343e9a0321140715
aqualinka <michaela.kovaja@gmail.com>
2021-05-23 21:40:45 +0100

    update winds km/h

--- src/Weather.js
+++ src/Weather.js
@@ -14,7 +14,7 @@ export default function Weather() {
         temperature: Math.round(response.data.main.temp),
         feel: Math.round(response.data.main.feels_like),
         description: response.data.weather[0].description,
-        wind: Math.round(response.data.wind.speed),
+        wind: Math.round((response.data.wind.speed)*18/5),
         humidity: response.data.main.humidity,
         icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
       });
@@ -79,7 +79,7 @@ export default function Weather() {
             <div className="col d-flex justify-content-evenly">
               <ul>
                 <li>humidity {weather.humidity}%</li>
-                <li>wind {weather.wind}m/s</li>
+                <li>wind {weather.wind}km/h</li>
               </ul>
             </div>  
           </div>
