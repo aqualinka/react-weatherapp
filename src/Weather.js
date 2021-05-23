57ec527eefc9f6f338ed39e6dbc3d5c86e0e1019
aqualinka <michaela.kovaja@gmail.com>
2021-05-24 00:38:20 +0100

    updated Weather function if statement, date,

--- src/Weather.js
+++ src/Weather.js
@@ -1,16 +1,18 @@
 import React, { useState} from "react";
 import axios from "axios";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import WeatherInfo from "./WeatherInfo";
 import "./Weather.css";
 
-export default function Weather() {
-    let [city, setCity] = useState(null);
-    let [weather, setWeather] = useState({});
+export default function Weather(props) {
+    const [city, setCity] = useState(props.defaultCity);
+    const [weatherData, setWeatherData] = useState({ready: false});
   
     function showWeather(response) {
-      console.log(response.data);
-      setWeather({
+     // console.log(response.data);
+      setWeatherData({
+        ready: true,
         city: response.data.name,
+        date: new Date(response.data.dt*1000),
         temperature: Math.round(response.data.main.temp),
         feel: Math.round(response.data.main.feels_like),
         description: response.data.weather[0].description,
@@ -22,23 +24,27 @@ export default function Weather() {
   
     function handleSubmit(event) {
       event.preventDefault();
-      let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=728f15542e17d610b9afb1e420062506&units=metric`;
-      axios.get(apiUrl).then(showWeather);
+      search();
+    
     }
   
     function updateCity(event) {
       setCity(event.target.value);
     }
-  
+    function search(){
+      let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=728f15542e17d610b9afb1e420062506&units=metric`;
+      axios.get(apiUrl).then(showWeather);
+    }
+  if(weatherData.ready){
     return (
       <div className="Weather">
-        <div className=" container mt-2 weather-app-container">
-          <div className="header text-center pt-2 pb-2">
-             Welcome to my Weather App
+        <div className="mt-2 weather-app-container">
+          <div className="header text-center py-2">
+             Welcome to Your Weather App
           </div>
 
-          <div className="mt-2">
-            <form className="search-form pb-2"onSubmit={handleSubmit}>
+          <div className="p-2">
+            <form className="search-form"onSubmit={handleSubmit}>
               <div className="row">
                 <div className="col-8">
                   <input
@@ -59,37 +65,16 @@ export default function Weather() {
             </form>
           </div>
 
-          <div className="row pb-2">
-            <div className="col text-center">
-              <h1 className="heading">{weather.city}</h1>
-              <div className="weather-temperature">
-                <img src={weather.icon} alt={weather.description} />
-                <span className="temp">{weather.temperature}ºC</span>
-              </div>
-            </div>
-          </div>
-    
-          <div className="row pb-2">
-            <div className="col d-flex justify-content-evenly">
-              <ul>
-                <li>{weather.description}</li>
-                <li>feel {weather.feel}ºC</li>
-              </ul>
-            </div>
-            <div className="col d-flex justify-content-evenly">
-              <ul>
-                <li>humidity {weather.humidity}%</li>
-                <li>wind {weather.wind}km/h</li>
-              </ul>
-            </div>  
-          </div>
-          <div className="coder text-center">
-          
-            <a href="https://github.com/aqualinka/react-weatherapp" target="_blank" rel="noreferrer">open-source-code </a>by Michaela Kovacova and hosted on <a href="https://happy-wing-013044.netlify.app" target="_blank"rel="noreferrer">Netlify</a>
-          </div>
+         <WeatherInfo data={weatherData}/>
+         
         </div>
       </div>   
     );
+  } else{
+    search();
+    return "Loading...";
+  }
+    
   
   
  
