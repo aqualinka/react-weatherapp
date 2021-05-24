d1c951722920bc5259920da4b8934e7a7850e6b7
aqualinka <michaela.kovaja@gmail.com>
2021-05-24 18:21:24 +0100

    added unit change to fahrenheit

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -1,11 +1,35 @@
-import React from "react";
+import React, { useState} from "react";
 import "./WeatherTemperature.css"
 
 export default function WeatherTemperature(props){
+   const [units,setUnits] = useState("celsius");
+   
+    function showFahrenheit(event){
+       event.preventDefault();
+       setUnits("fahrenheit");
+    }
+      
+   function showCelsius(event){
+    event.preventDefault();
+    setUnits("celsius");
+    }
 
-    return  (<div className="m-2">
-        <span className="temp">{props.celsius}</span>
-        <span className="unit" >ºC</span>
-        <a href="/" className="fahrenheit">F</a>
-        </div>);
+   if(units === "celsius"){
+    return  (
+        <div className="m-2">
+             <span className="temp">{Math.round(props.celsius)}</span>
+             <span className="unit" >ºC</span>
+             <a href="/" className="fahrenheit" onClick={showFahrenheit} >ºF</a>
+         </div>);
+   } else {
+    return  (
+        <div className="m-2">
+             <span className="temp">{Math.round(props.celsius*9/5+32)}</span>
+             <span className="unit" >ºF</span>
+             <a href="/" className="fahrenheit" onClick={showCelsius} >ºC</a>
+         </div>);
+   }
+   
+ 
+  
 }
